beae07e30a8dc0b18702f438ccb8fb997d97a6b4
Peter Esselius <pepp@me.com>
2019-11-14 13:23:38 +0100

    Let the user decide what files to include or exclude

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"path"
-	"strings"
-
 	"log"
 	"os"
 	"text/template"
@@ -13,29 +10,21 @@ import (
 	"github.com/joshdk/go-junit"
 )
 
-
 func main() {
-	dir, err := ioutil.ReadDir(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	fmt.Println("# Test results")
 
-	for _, file := range dir {
-		if strings.HasPrefix(file.Name(), "TEST") {
-			f, err := ioutil.ReadFile(path.Join(os.Args[1], file.Name()))
-			if err != nil {
-				log.Fatal(err)
-			}
+	for _, file := range os.Args[1:] {
+		f, err := ioutil.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			suites, err := junit.Ingest(f)
-			if err != nil {
-				log.Fatal(err)
-			}
+		suites, err := junit.Ingest(f)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-
-			tmpl, err := template.New("").Parse(`
+		tmpl, err := template.New("").Parse(`
 {{- range .}}
 ### {{.Name}}
 |Success|Test|
@@ -45,15 +34,15 @@ func main() {
 {{- end}}
 {{- end}}
 `)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			err = tmpl.Execute(os.Stdout, suites)
-			if err != nil {
-				log.Fatal()
-			}
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		err = tmpl.Execute(os.Stdout, suites)
+		if err != nil {
+			log.Fatal()
+		}
+
 	}
 
 }
